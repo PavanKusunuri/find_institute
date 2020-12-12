ac9f989e1243a7abdda977fca165b108c1d962db
Pavan <pavantejakusunuri@gmail.com>
2020-12-12 22:15:42 +0530

    Express set up for app

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-const express = require("express")
+const express = require("express");
+const dotenv = require("dotenv");
+
+//  Load env variables
+
+dotenv.config({ path: './config/config.env' })
+
+const app = express();
+
+const PORT = process.env.PORT || 5002;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
+
+// console.log("PORT" + PORT)
